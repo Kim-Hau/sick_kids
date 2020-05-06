8cffa1a0b0066dcb8cf4803ce562d1e75ea83120
Kim-Hau <61749817+Kim-Hau@users.noreply.github.com>
2020-05-05 20:57:27 -0400

    Update partition_data_window.py

--- partition_data_window.py
+++ partition_data_window.py
@@ -6,12 +6,13 @@ df = pd.read_csv("2018_02_03_o0_l3751_h100.csv", names = ["timestamp", "DTW Dist
 l = list()
 l2 = list()
 d = dict()
+length = 100
 
-for i in range(100):
-	l=list( df.iloc[i:i+863659-100,1])
+for i in range(length):
+	l=list( df.iloc[i:i+863659-length,1])
 	d[i] = pd.Series(l)
 
-for i in range (863659-100):
+for i in range (863659-length):
 	if (1517635237000<=df["timestamp"].values[i]<(1517635259000-100) or 1517643560000<=df["timestamp"].values[i]<(1517643585000-100) or 1517646987000<=df["timestamp"].values[i]<(1517647012000-100) or 1517647064000<=df["timestamp"].values[i]<(1517647080000-100) or 1517647657000<=df["timestamp"].values[i]<(1517647683000-100) or 1517659150000<=df["timestamp"].values[i]<(1517659171000-100) or 1517682815000<=df["timestamp"].values[i]<(1517682837000-100) or 1517683692000<=df["timestamp"].values[i]<(1517683713000-100) or 1517686866000<=df["timestamp"].values[i]<(1517686878000-100) or 1517687212000<=df["timestamp"].values[i]<(1517687233000-100) or 1517703734000<=df["timestamp"].values[i]<(1517703770000-100)):
 		l2.append(1)
 	else:
